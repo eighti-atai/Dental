758ae25736277ea157e3f1ec2d70daff99d660ba
isumarasa <isumarasa@yahoo.com>
2017-02-03 11:18:42 +0530

    Appointment Validator

--- src/main/java/com/atai/dental/module/enterp/service/AppointmentService.java
+++ src/main/java/com/atai/dental/module/enterp/service/AppointmentService.java
@@ -3,6 +3,7 @@ package com.atai.dental.module.enterp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.atai.dental.generic.service.AbstractService;
 import com.atai.dental.module.enterp.dao.AppointmentDao;
@@ -12,10 +13,18 @@ import com.atai.dental.module.enterp.model.AppointmentKey;
 @Service
 public class AppointmentService extends AbstractService<AppointmentKey, Appointment>{
 
+	private AppointmentDao dao;
+	
 	@Autowired
 	public AppointmentService(AppointmentDao dao) {
 		super(dao);
+		this.dao = dao;
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Transactional
+	public boolean AppointemtTimeExists(Appointment appointment){
+		return this.dao.AppointemtTimeExists(appointment);
 	}
 	
 }
