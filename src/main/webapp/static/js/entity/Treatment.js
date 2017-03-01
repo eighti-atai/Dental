5ea4aee0bd4af6a2dbbca5a4382a40db6c909096
nalakaJ <nalakae06105@gmail.com>
2017-03-01 16:19:22 +0530

    tr

--- src/main/webapp/static/js/entity/Treatment.js
+++ src/main/webapp/static/js/entity/Treatment.js
@@ -7,9 +7,9 @@ entityModule.factory('EntityService', ['$http', '$q', function($http, $q){
         record :{id:{treatmentId:'',patientId:''} , patientName:'', mttId:'', sttId:'',treatmentDesc:'', treatmentStat:'', treatmentPaidStat:'', treatmentDate:'',treatmentAmount:'',treatmentDiscount:'',treatmentTotal:'',treatmentPaid:'',treatmentImage:null,objid:null},
         emptyRecord :emptyRecord,
         lov :{patientId: 'Patient',mttId:'MainTreatmentType',sttId:'SubTreatmentType'},
-        lovRecord:{patientId:{patientId:'',patientName:'',patientAddress:'',patientIdNo:'',patientContactNo:'',patientGender:''}, mttId:{mttId:'',mttName:'',mttDescription:''}},
+        lovRecord:{patientId:{patientId:'',patientName:'',patientAddress:'',patientIdNo:'',patientContactNo:'',patientGender:''}, mttId:{mttId:'',mttName:'',mttDescription:''},sttId:{id:{sttId:'',mttId:''} , treatmentName:'', treatmentAmount:''}},
         lovTitles :{patientId: 'Patient',treatmentId: 'Treatment',mttId: 'Main Treatment Type',sttId: 'Sub Treatment Type'},
-        lovHeads :{patientId:{patientId:'Id',patientName:'Name',patientAddress:'Address',patientIdNo:'ID No',patientContactNo:'Contact No',patientGender:'Gender'},mttId:{mttId:'Id',mttName:'Name XX',mttDescription:'Description'}}
+        lovHeads :{patientId:{patientId:'Id',patientName:'Name',patientAddress:'Address',patientIdNo:'ID No',patientContactNo:'Contact No',patientGender:'Gender'},mttId:{mttId:'Id',mttName:'Name',mttDescription:'Description'},sttId:{id:{sttId:'Id',mttId:'Main Treatment Type Id'}, treatmentName:'Name', treatmentAmount:'Amount'}}
     };
     return entityRec;
     
